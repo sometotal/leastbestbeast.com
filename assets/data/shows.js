40334abaa59e6e43ca0be0ac12939461e43d0340
Joe Sepi <sepi@joesepi.com>
2015-03-06 21:34:42 -0500

    adding div st show

---
+++ assets/data/shows.js
@@ -0,0 +1,29 @@
+window.shows = [{
+  date: 'Friday, February 20, 2015',
+  time: '8PM',
+  venue: 'Don Pedro',
+  venue_site: '',
+  city: '',
+  state: '',
+  bands: [{
+    band: 'Risk/Reward',
+    site: ''
+  }, {
+    band: 'The Mess Around',
+    site: ''
+  }, {
+    band: 'The Walk Ons',
+    site: ''
+  }, {
+    band: 'We Ride On',
+    site: ''
+  }],
+  cover: '',
+  age: '',
+  poster: '',
+  links: [{
+    name: 'Facebook Event',
+    link: 'https://www.facebook.com/events/1534001653554644/'
+  }]
+
+}];
